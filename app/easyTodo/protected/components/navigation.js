cbaf68ac8ece3acfd24c3612e56bce260db444b1
GabrielEllie <gabarill35@gmail.com>
2024-12-09 03:22:40 -0700

    fixed label for UI
    alt is properly described and changed colors of navigation

--- app/easyTodo/protected/components/navigation.js
+++ app/easyTodo/protected/components/navigation.js
@@ -7,14 +7,14 @@ export function Navigation({setPageFunc}) {
     
     return (
         <div className="flex flex-row items-center absolute bottom-0 w-screen h-20">
-                <div className="flex w-full h-full bg-red-500 items-center align-middle justify-center">  
+                <div className="flex w-full h-full bg-lime-800 items-center align-middle justify-center text-black">  
                     <button className="flex flex-row"
                         onClick={handleOverviewPress}
                     >
                         <img 
                             // src="./images/overview.png" 
                             src="https://uxwing.com/wp-content/themes/uxwing/download/web-app-development/dashboard-line-icon.png" 
-                            alt="picture of a magnifying glass"
+                            alt="picture of a circular meter"
                             className="h-full w-full mr-2 max-h-10 max-w-10" 
                         />  
                         <h3 className="text-3xl">Overview</h3>
@@ -27,7 +27,7 @@ export function Navigation({setPageFunc}) {
                         <img 
                             // src="./images/overview.png" 
                             src="https://uxwing.com/wp-content/themes/uxwing/download/tools-equipment-construction/gear-icon.png" 
-                            alt="picture of a magnifying glass"
+                            alt="picture of a gear icon"
                             className="h-full w-full mr-2 max-h-10 max-w-10 " 
                         />  
                         <h3 className="text-3xl">Manage</h3>
